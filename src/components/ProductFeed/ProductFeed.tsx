9bb9b1ec3cc256c3591b152cd1d721600e329b83
Ana <anastasy.d@gmail.com>
2021-06-22 21:48:53 +0200

    Fetch product data from server and show it in the app

--- src/components/ProductFeed/ProductFeed.tsx
+++ src/components/ProductFeed/ProductFeed.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import styles from './ProductFeed.module.css';
+import styles from "./ProductFeed.module.css";
+import { useEffect, useState } from "react";
+import { Product } from "../../interfaces/app-interfaces";
 
-const ProductFeed = () => (
-  <div className={styles.ProductFeed} data-testid="ProductFeed">
-    ProductFeed Component
-  </div>
-);
+export default function ProductFeed() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default ProductFeed;
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const result = await (
+        await fetch("http://localhost:3000/productdata")
+      ).json();
+      setProducts(result);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className={styles.ProductFeed} data-testid="ProductFeed">
+      {products.map((product: Product, i: number) => {
+        return (
+          <div className={styles.gridItem} key={i}>
+            <h4>{product.name || "Name is empty"}</h4>
+            <div>{product.price ? `€${product.price}` : ""}</div>
+            <div className={styles.Category}>{product.category}</div>
+            <div>
+              {product.image && <img src={product.image} alt={product.name} />}
+              {product.description || "Description is empty"}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/interfaces/app-interfaces.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  name?: string;
+  price?: string;
+  description?: string;
+  category?: string;
+  image?: string;
+}
